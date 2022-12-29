2c29920120bc456e90e098cab506b331a388ce08
LAPTOP-EVVG1K95\Alban Hashani <martin.na90@gmail.com>
2022-12-29 21:52:49 +0100

    Testing pusher basic installation, message working to pusher

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Pusher from "pusher-js";
 
 function App() {
+
+  Pusher.logToConsole = true;
+
+  const pusher = new Pusher('96f23e17a31c2ce9d24d"', {
+    cluster: "eu",
+  });
+
+  var channel = pusher.subscribe("my-channel");
+  channel.bind("my-event", function (data) {
+    alert(JSON.stringify(data));
+    console.log(data);
+  });
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-    <p>
-      Hej
-    </p>
+        <h1>Pusher Test</h1>
+        <p>
+          Try publishing an event to channel <code>my-channel</code>
+          with event name <code>my-event</code>.
+        </p>
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,11 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const Pusher = require("pusher");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+
+require('dotenv').config()
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+const app = express();
+const port = 3001;
+
+const pusher = new Pusher({
+  appId: process.env.APP_ID,
+  key: process.env.KEY,
+  secret: process.env.SECRET,
+  cluster: "eu",
+  useTLS: true
+});
+
+
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!!!!')
+  res.send('Hello World!')
+  pusher.trigger("my-channel", "my-event", {
+    message: "hello world"
+  });
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, () => console.log("Server started"));
