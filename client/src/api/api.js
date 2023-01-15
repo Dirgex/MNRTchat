7efa706d665a706099ef2bd917f3314c4c85f920
LAPTOP-EVVG1K95\Alban Hashani <martin.na90@gmail.com>
2023-01-15 16:01:28 +0100

    deploy client

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //Api call to send our message to the message endpoint in backend
 export const sendMessageApi = async (msg) => {
   try {
-    await axios.post("http://localhost:3001/message", msg);
+    await axios.post("https://mnrtchat-server.onrender.com/message", msg);
   } catch (error) {
     alert(error.message + "Something went wrong with the message");
   }
--- client/src/pages/chat.js
+++ client/src/pages/chat.js
@@ -34,7 +34,7 @@ const Chat = () => {
           userid: userid,
           user: user,
         },
-        endpoint: "http://localhost:3001/pusher/user-auth",
+        endpoint: "https://mnrtchat-server.onrender.com//pusher/user-auth",
       },
 
       channelAuthorization: {
@@ -42,7 +42,7 @@ const Chat = () => {
           userid: userid,
           user: user,
         },
-        endpoint: "http://localhost:3001/pusher/auth",
+        endpoint: "https://mnrtchat-server.onrender.com//pusher/auth",
       },
     });
 
