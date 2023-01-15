49ce24d73272547b3cfff6cb02ecaff9ffccb4df
LAPTOP-EVVG1K95\Alban Hashani <martin.na90@gmail.com>
2023-01-15 16:24:22 +0100

    new port for backend

--- client/public/env.js
+++ client/public/env.js
@@ -1,3 +1,1 @@
-window.env = {
-  "PUSHERKEY": "96f23e17a31c2ce9d24d"
-};
+window.env = {};
--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ require("dotenv").config({
 });
 
 const app = express();
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 5000;
 
 //Create new pusher instance
 const pusher = new Pusher({
